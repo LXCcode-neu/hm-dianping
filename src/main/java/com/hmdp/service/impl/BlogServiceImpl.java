8768ec4901de9bd447f605055e11444dd3fa37cd
Turlin <2936690131@qq.com>
2025-12-18 18:34:06 +0800

    通过滚动查询实现关注推送

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -5,18 +5,23 @@ import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.ScrollResult;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.Follow;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IFollowService;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -37,6 +42,8 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     private UserServiceImpl userService;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IFollowService followService;
     @Override
     public Result queryHotBlog(Integer current) {
         // 根据用户查询
@@ -72,6 +79,80 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
                 .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
                 .collect(Collectors.toList());
         return Result.ok(userDTOs);
+    }
+
+    @Override
+    public Result saveBlog(Blog blog) {
+        // 获取登录用户
+        UserDTO user = UserHolder.getUser();
+        blog.setUserId(user.getId());
+        // 保存探店博文
+        boolean isSuccess =save(blog);
+        if (!isSuccess) {
+            return Result.fail("发布失败！");
+        }
+        //查询该作者所有粉丝
+        List<Follow> follows = followService.query().eq("follow_user_id", user.getId()).list();
+        //推送给该id所有粉丝
+        for (Follow follow : follows) {
+            Long followId = follow.getUserId();
+            String key = "feed" + followId;
+            //保存到redis
+            stringRedisTemplate.opsForZSet().add(key,blog.getId().toString(),  System.currentTimeMillis());
+        }
+        // 返回id
+        return Result.ok(blog.getId());
+
+    }
+
+    @Override
+    public Result queryBlogOfFollow(Long max, Integer offset) {
+        //获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        //查询收件箱
+        String key = "feed" + userId;
+         Set<ZSetOperations.TypedTuple<String>> typedTuples= stringRedisTemplate.opsForZSet()
+                .reverseRangeByScoreWithScores(key,0,max,offset,2);
+         if(typedTuples==null||typedTuples.isEmpty()){
+             return Result.ok();
+         }
+        //解析数据 blogId minTime offset
+        List<Long>ids=new ArrayList<>(typedTuples.size());
+         long minTime = 0;
+         int os=1;
+        for(ZSetOperations.TypedTuple<String> tuple:typedTuples){
+            //获取id
+            String idStr = tuple.getValue();
+            ids.add(Long.valueOf(idStr));
+            //获取offset值
+            long time = tuple.getScore().longValue();
+            if(time==minTime){
+                os++;
+            }else{
+                minTime=time;
+                os=1;
+            }
+        }
+        //根据id查询blog
+        String idStr=StrUtil.join(",",ids);
+        List<Blog>blogs=query()
+                .in("id",ids).last("order by field(id," +idStr +")").list();
+        for (Blog blog : blogs) {
+            //查询有关用户
+            Long Id = blog.getUserId();
+            User user = userService.getById(Id);
+            blog.setName(user.getNickName());
+            blog.setIcon(user.getIcon());
+            //查询blog是否被点赞
+            isBlogLiked(blog);
+
+        }
+        //封装并返回
+        ScrollResult r=new ScrollResult();
+        r.setList(blogs);
+        r.setOffset(os);
+        r.setMinTime(minTime);
+        return Result.ok(r);
     }
 
     @Override
